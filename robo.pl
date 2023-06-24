cae7071efa54b65b15adb4a7d0be10a57bf4bbe4
Camila Ferreira <camilaferreira@pop-os.localdomain>
2023-06-24 02:16:59 -0300

    Tabuleiro e movimentacoes do robo

--- robo.pl
+++ robo.pl
@@ -1,59 +1,86 @@
-% Sala com 48 espaços , formando uma amtrix 6x8, o é objeto, l é limpo/livre e s é sujo
-sala([
-    [l, s, l, l, o, s, l, o],
-	[l, s, s, l, o, l, s, o],
-	[s, o, l, l, s, o, o, l],
-	[o, l, s, o, s, l, s, l],
-	[s, o, l, s, o, s, l, o],
-	[l, l, s, o, l, s, o, l]
-    ]).
-% Posição inicial do robô
-p(0, 0).
+% Inicia matriz com linhas e colunas definidas pelo usuário, as sujeiras ou obstáculos são adicionados pelo "s" e pelo "o", respectivamente.
+% Eles serão adicionados em situações randômicas, enquanto os lugares limpos são representados por "l".
+inicia_sala(Matriz, Linhas, Colunas,Sala) :-
+    length(Matriz, Linhas),
+    inicia_linhas(Matriz, Colunas),
+    move_robo(Matriz, 0, 0, Sala),
+    imprime_sala(Sala). 
 
-% Movimenta o robô para cima
-move_c :-
-	posicao(X, Y),
-	Y1 is Y - 1,
-	sala(Sala),
-	nth0(Y1, Sala, Linha),
-	nth0(X, Linha, limpo),
-	retract(posicao(X, Y)),
-	assert(posicao(X, Y1)).
+inicia_linhas([], _).
+inicia_linhas([Linha | Corpo], Colunas) :-
+    length(Linha, Colunas),
+    preenche_sala(Linha),
+    inicia_linhas(Corpo, Colunas).
 
-% Movimenta o robô para baixo
-move_b :- ...
-    
-% Movimenta o robô para esquerda
-move_e :- ...
-    
-% Movimenta o robô para direita
-move_direita :- ...
-    
-% Movimenta o robô para diagonal esquerda baixo
-move_eb :- ...
+define_valor(Resultado) :-
+    random(0, 3, Escolha),
+    (Escolha = 0 -> Resultado = 'l' ;
+     Escolha = 1 -> Resultado = 's' ;
+     Resultado = 'o').
 
-% Movimenta o robô para diagonal esquerda cima
-move_ec :- ...
+preenche_sala([]).
+preenche_sala([Cabeca | Corpo]) :-
+    define_valor(Valor),
+    Cabeca = Valor,
+    preenche_sala(Corpo).
 
-% Movimenta o robô para diagonal direita baixo
-move_db :- ...
+%----------------------------Robô--------------------------------------
+% Movimento do robô
+move_robo(Sala, X, Y, NovaSala) :-
+    nth0(X, Sala, Linha), %Quero pegar a linha na posição X da Sala 
+    replace(Y, Linha, 'r', NovaLinha), %Faco o replace apenas nessa linha, na posicao Y pelo r, que é o robo
+    replace(X, Sala, NovaLinha, NovaSala). %Aplico o replace novamente na matriz, trocando a posição das linhas
 
-% Movimenta o robô para diagonal direita cima
-move_dc :- ...
-    
-% Limpa a posição atual do robô
-limpa :-
-posicao(X, Y),
-sala(Sala),
-nth0(Y, Sala, Linha),
-nth0(X, Linha, sujo),
-retract(nth0(Y, Sala, Linha)),
-assert(nth0(Y, Sala, [X=limpo | RestoLinha])).
-% Ciclo principal do robô
-robot :-
-posicao(X, Y),
-limpa,
-move_cima;
-move_baixo;
-move_esquerda;
-move_direita.
+replace(Indice, Lista, Elemento, NovaLista) :-
+    nth0(Indice, Lista, _, Temporaria),
+    nth0(Indice, NovaLista, Elemento, Temporaria).
+
+limpa_posicao_antiga(Sala, XAntigo, YAntigo, NovaSala):-
+    nth0(XAntigo, Sala, Linha), 
+    replace(YAntigo, Linha, 'l', NovaLinha), 
+    replace(XAntigo, Sala, NovaLinha, NovaSala). 
+
+d(Sala, XAtual, YAtual, NovaSala):- %Direita
+    limpa_posicao_antiga(Sala,XAtual,YAtual,SalaTemporaria),
+    NovoY is YAtual+1,
+    move_robo(SalaTemporaria, XAtual, NovoY, NovaSala).
+
+e(Sala, XAtual, YAtual, NovaSala):- %Esquerda
+    limpa_posicao_antiga(Sala,XAtual,YAtual,SalaTemporaria),
+    NovoY is YAtual-1,
+    move_robo(SalaTemporaria, XAtual, NovoY, NovaSala).
+
+f(Sala, XAtual, YAtual, NovaSala):- %Frente
+    limpa_posicao_antiga(Sala,XAtual,YAtual,SalaTemporaria),
+    NovoX is XAtual+1,
+    move_robo(SalaTemporaria, NovoX, YAtual, NovaSala).
+
+t(Sala, XAtual, YAtual, NovaSala):- %Tras
+    limpa_posicao_antiga(Sala,XAtual,YAtual,SalaTemporaria),
+    NovoX is XAtual-1,
+    move_robo(SalaTemporaria, NovoX, YAtual, NovaSala).
+
+d(Sala, XAtual, YAtual, NovaSala):- %Diagonal 
+    limpa_posicao_antiga(Sala,XAtual,YAtual,SalaTemporaria),
+    NovoX is XAtual+1,
+    NovoY is YAtual+1,
+    move_robo(SalaTemporaria, NovoX, NovoY, NovaSala).
+
+
+%----------------------------------- Algorítmos e Heurísticas----------------
+%TODO
+%------------------------------------Imprime a sala-----------------------
+imprime_sala(Sala) :-
+    imprime_linhas(Sala).
+
+imprime_linhas([]).
+imprime_linhas([Linha | Corpo]) :-
+    imprime_elementos(Linha),
+    nl,
+    imprime_linhas(Corpo).
+
+imprime_elementos([]).
+imprime_elementos([Elemento | Corpo]) :-
+    write(Elemento),
+    write(' '),
+    imprime_elementos(Corpo).
